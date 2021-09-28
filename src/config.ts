559b8a10d5e349bd460708f6a0dd5dea6084f078
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2021-09-28 20:40:05 +0800

    Update template

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 
-import IConfig, { IMySQL } from './interfaces/config.interface'
+import IConfig from './interfaces/config.interface'
 
 dotenv.config()
 
@@ -13,10 +13,6 @@ dotenv.config()
 
 const SETTINGS_DEVELOPMENT: IConfig = {
   name: 'development',
-  use_database: false,
-  wipe_db_on_start: false,
-  migrate_db_on_start: false,
-  seed_db_on_start: false,
   public_url: 'http://localhost:3000',
   client_url: 'http://localhost:3000',
   cors: {
@@ -38,10 +34,6 @@ const SETTINGS_DEVELOPMENT: IConfig = {
 
 const SETTINGS_MOCHA_TESTING: IConfig = {
   name: 'mocha-testing',
-  use_database: false,
-  wipe_db_on_start: false,
-  migrate_db_on_start: false,
-  seed_db_on_start: false,
   public_url: 'http://localhost:3000',
   client_url: 'http://localhost:3000',
   cors: {
@@ -63,10 +55,6 @@ const SETTINGS_MOCHA_TESTING: IConfig = {
 
 const SETTINGS_PRODUCTION: IConfig = {
   name: 'production',
-  use_database: false,
-  wipe_db_on_start: false,
-  migrate_db_on_start: false,
-  seed_db_on_start: false,
   public_url: 'http://localhost:3000',
   client_url: 'http://localhost:3000',
   cors: {
@@ -88,10 +76,6 @@ const SETTINGS_PRODUCTION: IConfig = {
 
 const SETTINGS_TESTING: IConfig = {
   name: 'testing',
-  use_database: false,
-  wipe_db_on_start: false,
-  migrate_db_on_start: false,
-  seed_db_on_start: false,
   public_url: 'http://localhost:3000',
   client_url: 'http://localhost:3000',
   cors: {
@@ -112,31 +96,15 @@ const SETTINGS_TESTING: IConfig = {
  */
 
 export const getSettings = (): IConfig | null => {
-  if (process.env.NODE_ENV === 'dev') return SETTINGS_DEVELOPMENT
-  if (process.env.NODE_ENV === 'mocha') return SETTINGS_MOCHA_TESTING
-  if (process.env.NODE_ENV === 'prod') return SETTINGS_PRODUCTION
-  if (process.env.NODE_ENV === 'testing') return SETTINGS_TESTING
+  if (process.env.API_ENV === 'dev') return SETTINGS_DEVELOPMENT
+  if (process.env.API_ENV === 'mocha') return SETTINGS_MOCHA_TESTING
+  if (process.env.API_ENV === 'prod') return SETTINGS_PRODUCTION
+  if (process.env.API_ENV === 'testing') return SETTINGS_TESTING
   return null
-}
-
-/**
- * @property MySQL_CONNECTION
- * @description MySQL connection settings used by Knex
- * @implements IMySQL
- * @returns object
- */
-
-export const MYSQL_CONNECTION: IMySQL = {
-  host: process.env.MYSQL_HOSTNAME ? process.env.MYSQL_HOSTNAME : '127.0.0.1',
-  user: process.env.MYSQL_USERNAME ? process.env.MYSQL_USERNAME : 'root',
-  password: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : '',
-  database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : 'Verci',
 }
 
 export const APP_PORT = process.env.PORT || 7777
 export const RUNTIME_MODE = getSettings() != null ? getSettings()?.name : null
-export const WIPE_DB_ON_START = getSettings() != null ? getSettings()?.wipe_db_on_start : null
-export const MIGRATE_DB_ON_START = getSettings() != null ? getSettings()?.migrate_db_on_start : null
 export const PUBLIC_URL = getSettings() != null ? getSettings()?.public_url : null
 export const CLIENT_URL = getSettings() != null ? getSettings()?.client_url : null
 export const CORS_OPTIONS = getSettings() != null ? getSettings()?.cors : null
--- src/interfaces/config.interface.ts
+++ src/interfaces/config.interface.ts
@@ -2,18 +2,7 @@ import { CorsOptions } from 'cors'
 
 export default interface IConfig {
   name: string
-  use_database: boolean
-  wipe_db_on_start: boolean
-  migrate_db_on_start: boolean
-  seed_db_on_start: boolean
   public_url: string
   client_url: string
   cors: CorsOptions
-}
-
-export interface IMySQL {
-  host: string
-  user: string
-  password: string
-  database: string
 }
